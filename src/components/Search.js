6bb9528a558b57942810d84c6cdfe7d8dc6483fd
Jack Hipperson <jackhipperson@hotmail.com>
2024-06-18 22:05:00 +0100

    Dev

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { fetchFilms } from "../util/fetch-http";
 import Results from "./Results";
 
@@ -6,24 +6,58 @@ const Search = () => {
   // enteredSearch is a state that holds the users search term. searchResults will hold the results from the API call
   const [enteredSearch, setEnteredSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [apiError, setApiError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  let errorText;
+
+  console.log(searchResults);
+  console.log(apiError);
 
   // function called with every letter entered in the search bar to update enteredSearch state
-  const handleSearch = async (event) => {
+  const handleSearch = (event) => {
     setEnteredSearch(event.target.value);
   };
 
-  // useEffect that calls the API once search characters is equal to or more than 3 and saves the results in the searchResults State. If less than 3 chars, or user has deleted chars, the results clear
-  useEffect(() => {
+  // useEffect that calls the API and saves the results in the searchResults State.
+
+  const fetchSearchResults = useCallback(async () => {
+    setIsLoading(true);
+    setApiError(null);
     if (enteredSearch.length >= 3) {
-      async function fetchSearchResults() {
+      try {
         let apiResult = await fetchFilms(enteredSearch);
-        setSearchResults(apiResult);
+        if (apiResult.status !== 200) {
+          setApiError(apiResult.message);
+          console.log(apiResult);
+        } else {
+          setSearchResults(apiResult.data.results);
+          setApiError(null);
+        }
+      } catch (error) {
+        setApiError(error);
+        setSearchResults([]);
       }
-      fetchSearchResults();
     } else {
       setSearchResults([]);
     }
-  }, [enteredSearch]);
+    setIsLoading(false);
+  },[enteredSearch]);
+
+  useEffect(() => {
+    fetchSearchResults(enteredSearch);
+  }, [enteredSearch, fetchSearchResults]);
+
+  if (apiError) {
+    errorText = (
+      <p className="text-red-800 p-2">
+        Error: {apiError || "There has been an error"} - Please contact support.
+      </p>
+    );
+  } else {
+    errorText = "";
+  }
+
+  console.log(searchResults);
 
   return (
     <div className="max-w-6xl mx-auto my-10 text-center">
@@ -43,7 +77,14 @@ const Search = () => {
           </div>
         </button> */}
       </form>
-      <Results searchResults={searchResults} />
+      {isLoading && <p>Loading...</p>}
+      {apiError ? (
+        { errorText }
+      ) : searchResults ? (
+        <Results searchResults={searchResults} />
+      ) : (
+        <p>Enter 3 or more letters to start a search.</p>
+      )}
     </div>
   );
 };
--- src/util/fetch-http.js
+++ src/util/fetch-http.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const fetchFilms = async (search) => {
-  let res = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${API_KEY}`)
-
-  return res.data.results;
+  let res = await axios.get(
+    `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${API_KEY}`
+  );
+  return res;
 };
