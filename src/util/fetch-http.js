7ab2148e01921e66b9aed90d48ee34cebfb28ef7
Jack Hipperson <jackhipperson@hotmail.com>
2024-06-19 22:01:46 +0100

    Dev

--- src/util/fetch-http.js
+++ src/util/fetch-http.js
@@ -1,9 +1,7 @@
-import axios from "axios";
-
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const fetchFilms = async (search) => {
-  let res = await axios.get(
+  let res = await fetch(
     `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${API_KEY}`
   );
   return res;
