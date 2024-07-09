8db1508523fe35834e7a9cf6d24915914aafc9de
Jack Hipperson <jackhipperson@hotmail.com>
2024-07-09 21:51:59 +0100

    Dev

--- src/components/Help.js
+++ src/components/Help.js
@@ -13,7 +13,7 @@ const Help = () => {
       </div>
 
       <div className="p-2">
-        <p className="text- text-base ">
+        <p className="text-base ">
           Welcome to the Film App! You can Search for films, save the films you
           love to your Favourites, get suggestions based on your Favourites and
           save them in your WatchList for later!
@@ -22,30 +22,29 @@ const Help = () => {
       <div className="flex flex-col mt-10 mx-auto items-center text-sm">
         <HelpItem icon={searchIcon} title="Search">
           Search for films by entering at least 3 characters into the search
-          bar. You can save to favourites by clicking the heart{" "}
+          bar. You can save to favourites by clicking the heart
           <HelpIcon icon={favIcon} title="Heart" /> icon or add to your
-          WatchList by clicking the list{" "}
+          WatchList by clicking the list
           <HelpIcon icon={watchListIcon} title="List" /> icon
         </HelpItem>
         <HelpItem icon={favIcon} title="Favourites">
           You can add or remove any film from your favourites list by clicking
           the heart <HelpIcon icon={favIcon} title="Heart" /> icon. This will
           help populate your Recommended films, see these by clicking
-          Recommended <HelpIcon icon={recommendedIcon} title="Recommended" />{" "}
+          Recommended <HelpIcon icon={recommendedIcon} title="Recommended" />
         </HelpItem>
         <HelpItem icon={recommendedIcon} title="Recommended">
           Recommendations are based on the films that you have saved in your
           Favourites <HelpIcon icon={favIcon} title="Heart" /> list. Browse
           through films and add any that are interesting to your Watch List by
-          clicking the list <HelpIcon icon={watchListIcon} title="List" />{" "}
+          clicking the list <HelpIcon icon={watchListIcon} title="List" />
         </HelpItem>
 
         <HelpItem icon={watchListIcon} title="Watch List">
-          {" "}
           You can add or remove any film from your watch list by clicking the
           list <HelpIcon icon={watchListIcon} title="List" /> icon. Once you
           have watched the film you can remove by clicking the icon again, and
-          don't forget to click the heart{" "}
+          don't forget to click the heart
           <HelpIcon icon={favIcon} title="Heart" /> icon to add it to your
           Favourites and make your recommendations better!
         </HelpItem>
